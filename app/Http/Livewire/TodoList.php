b17b6fffaea0da89c9abdb9568ffe1ee80aacdde
JabedHossainSwe <jabedhossain.swe@gmail.com>
2023-10-10 01:55:05 +0600

    add todos list and create todo feature

--- app/Http/Livewire/TodoList.php
+++ app/Http/Livewire/TodoList.php
@@ -28,6 +28,9 @@ class TodoList extends Component
     }
     public function render()
     {
-        return view('livewire.todo-list');
+
+        return view('livewire.todo-list', [
+            'todos' => Todo::latest()->get()
+        ]);
     }
 }
