cfd0d587a4b6adc71b7c80627742526d9bbce7b0
JabedHossainSwe <jabedhossain.swe@gmail.com>
2023-10-10 01:44:20 +0600

    validation done

--- app/Http/Livewire/TodoList.php
+++ app/Http/Livewire/TodoList.php
@@ -2,14 +2,29 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Todo;
 use Livewire\Component;
 
 class TodoList extends Component
 {
     public $name;
+    public $search;
+    protected $rules = [
+        'name' => 'required|min:3|max:50',
+    ];
     public function create()
     {
-        dd('sdaf');
+        //validate
+        $validated = $this->validateOnly('name');
+
+        //create the todo
+        Todo::create($validated);
+
+        // clear the input
+        $this->reset('name');
+
+        //send flash message
+        session()->flash('success', 'Created');
     }
     public function render()
     {
